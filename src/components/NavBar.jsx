2e515d6bcb57834ddde45abde83f5497e2580103
Razdee01 <rahmanrajdee@gmail.com>
2025-12-13 16:45:39 +0600

    mobile all contest fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -69,7 +69,7 @@ const NavBar = () => {
             </li>
             <li>
               <NavLink
-                to="/contests"
+                to="/all-contests"
                 className={({ isActive }) =>
                   `font-semibold ${isActive ? "text-purple-500" : ""}`
                 }
